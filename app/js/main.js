7d62a96474a56d4ba7e1a412db6701c0e04b5e9c
dxpression-lxrd <iloumeoww@gmail.com>
2021-09-19 19:24:52 +0300

    Сделал блок с лучшими товарами

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,6 @@ $(function(){
     prevArrow: '<img class="slider__arrow-left" src="images/slider/arrow-left.svg" alt="Стрелка влево">',
     nextArrow: '<img class="slider__arrow-right" src="images/slider/arrow-right.svg" alt="Стрелка вправо">',
   });
+
+  var mixer = mixitup('.best-goods__list');
 });
