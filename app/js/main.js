31abb0238b267c624b8a56c6e61b70c9e2e0075c
dxpression-lxrd <iloumeoww@gmail.com>
2021-09-19 13:01:19 +0300

    Добавил слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-
+  $('.slider').slick({
+    prevArrow: '<img class="slider__arrow-left" src="images/slider/arrow-left.svg" alt="Стрелка влево">',
+    nextArrow: '<img class="slider__arrow-right" src="images/slider/arrow-right.svg" alt="Стрелка вправо">',
+  });
 });
