f62ccfd89933e956f4da20f5b7b21372102d1e57
dxpression-lxrd <iloumeoww@gmail.com>
2021-09-24 16:06:12 +0300

    Добавил выпадающее меню товаров

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,11 @@ $(function(){
     nextArrow: '<svg class="intro-slider__arrow-right" width="62" height="62" viewBox="0 0 62 62" fill="none" xmlns="http://www.w3.org/2000/svg"><rect x="0.5" y="0.5" width="61" height="61" rx="6.5" fill="white" fill-opacity="0.8" stroke="#C1C1C1"/><path d="M39.9641 29.9851L23.9508 15.3921C23.4254 14.8693 22.5731 14.8693 22.0477 15.3921C21.5222 15.9149 21.5222 16.7631 22.0477 17.2859L37.0954 31L22.049 44.7141C21.5235 45.2369 21.5235 46.0852 22.049 46.6079C22.5745 47.1307 23.4267 47.1307 23.9521 46.6079L39.9654 32.0149C40.2455 31.7362 40.3656 31.3668 40.3469 31.0014C40.3643 30.6346 40.2443 30.2652 39.9641 29.9851Z" fill="#505050"/></svg>',
   });
 
+  $('.header__catalog-btn').on('click', function() {
+    $(this).toggleClass('header__catalog-btn--active');
+    $('.header__catalog-item').toggleClass('header__catalog-item--active');
+  });
+
   var mixitup1 = document.querySelector('[data-ref="mixitup-1"]');
   var mixitup2 = document.querySelector('[data-ref="mixitup-2"]');
 
